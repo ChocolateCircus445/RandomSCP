3736e0cc6c06b8be56058f0698bf1425c9a6a8a4
ChocolateCircus445 <41135752+ChocolateCircus445@users.noreply.github.com>
2020-08-08 12:41:54 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
-
+# The number after the -p is the port this will be running on.
+# I don't think 5168 will conflict with anything, but you can
+# change the port number if you want to.
+export FLASK_APP=randomscp.py
+python3 -m flask -p 5168
